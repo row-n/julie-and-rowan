eacfb05184e31a5d3ff027db3c8a0a646c7878c3
Rowan Parkinson <rowan.parkinson@gmail.com>
2019-11-05 17:32:14 +1100

    Adding media queries and packages need to excute.

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,8 @@ module.exports = {
   ident: 'postcss',
   plugins: {
     'postcss-normalize': {},
+    'postcss-nested': {},
+    'postcss-custom-media': {},
     'postcss-preset-env': {},
     'autoprefixer': {},
     'cssnano': {},
