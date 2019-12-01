ec226fb6339eaba10858410d407bae38ede787fb
Rowan Parkinson <rowan.parkinson@gmail.com>
2019-12-01 21:50:19 +1100

    Targeting Toggle class for each element

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -3,7 +3,8 @@
 import Grid from './components/grid';
 import Toggle from './components/toggle';
 
-const toggleEl = document.querySelector('[data-toggle]');
+const toggleEl = document.querySelectorAll('[data-toggle]');
+const toggleArr = [];
+toggleEl.forEach((toggle) => toggleArr.push(new Toggle(toggle)));
 
 (() => new Grid())();
-(() => new Toggle(toggleEl))();
